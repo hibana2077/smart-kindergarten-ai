95d81d34dc3cd0dbc6c17e5daa269f453fe86169
hibana2077 <hibana2077@gmail.com>
2024-11-05 11:03:45 +0800

    Refactor download script to use variables for file name and URL

--- src/id_verification/exp/download_models.sh
+++ src/id_verification/exp/download_models.sh
@@ -1,1 +1,4 @@
-wget https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task
+FILE="hand_landmarker.task"
+URL="https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task"
+
+wget -O $FILE $URL
